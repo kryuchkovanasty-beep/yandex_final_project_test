e5332d2272b9e6b6a7d0d21a575f8cfd2f8ee74f
kryuchkovanasty-beep <kryuchkova.nasty@gmail.com>
2026-08-24 17:50:03 +0700

    Обновлен скриншот

--- get_order_by_track_test.py
+++ get_order_by_track_test.py
@@ -1,20 +1,19 @@
 # Крючкова Анастасия, 46-я когорта — Финальный проект. Инженер по тестированию плюс
 
+import pytest
+
 import data
 import sender_stand_request
 
-def test_get_order_by_track():
-    #Проверяет что созданный заказ можно получить по его номеру.
-    response_create = sender_stand_request.post_new_order(
-        data.order_body
-    )
 
-    assert response_create.status_code == 201
+@pytest.fixture
+def track():
+    response_create = sender_stand_request.post_new_order(data.order_body)
+    return response_create.json()["track"]
 
-    track = response_create.json()["track"]
 
+def test_get_order_by_track(track):
+    #Проверяет, что по треку заказа можно получить данные о заказе.
     response_get = sender_stand_request.get_order_by_track(track)
 
     assert response_get.status_code == 200
-    print(f"Код ответа при получении заказа: {response_get.status_code}")
-    print("Тест успешно пройден.")
